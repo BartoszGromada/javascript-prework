e02d35f01667a9356c1eab749c0ee247e07f4460
Bartosz Gromada <bartosz.k.gromada@gmail.com>
2019-09-13 11:27:24 +0200

    new_file_functions_and_scripts

---
+++ js/script.js
@@ -0,0 +1,45 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+	computerMove = 'kamień';
+}	else if (randomNumber == 2) {
+	computerMove = 'papier';
+}	else if (randomNumber == 3) {
+	computerMove = 'nożyczki';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1') {
+    playerMove = 'kamień';
+}    else if (playerInput == '2') {
+	playerMove = 'papier';
+}	else if (playerInput == '3') {
+	playerMove = 'nożyczki';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove == 'kamień' && playerMove == 'papier') {printMessage('Ty wygrywasz');
+} else if (computerMove == 'kamień' && playerMove == 'nożyczki') {printMessage('Komputer wygrywa');
+} else if (computerMove == 'kamień' && playerMove == 'kamień') {printMessage('Remis!');
+} else if (computerMove == 'kamień' && playerMove == 'nieznany ruch') {printMessage('brak/błędna wartość');
+} else if (computerMove == 'papier' && playerMove == 'papier') {printMessage('Remis !');
+} else if (computerMove == 'papier' && playerMove == 'nożyczki') {printMessage('Ty wygrywasz');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {printMessage('Komputer wygrywa');
+} else if (computerMove == 'papier' && playerMove == 'nieznany ruch') {printMessage('brak/błędna wartość');
+} else if (computerMove == 'nożyczki' && playerMove == 'papier') {printMessage('Komputer wygrywa');
+} else if (computerMove == 'nożyczki' && playerMove == 'nożyczki') {printMessage('Remis !');
+} else if (computerMove == 'nożyczki' && playerMove == 'kamień') {printMessage('Ty wygrywasz');
+} else if (computerMove == 'nożyczki' && playerMove == 'nieznany ruch') {printMessage('brak/błędna wartość');
+}
